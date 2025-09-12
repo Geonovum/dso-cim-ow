eb43b7c93498af681da59b6a273d5afd561e1d6c
Wilko Quak <w.quak@geonovum.nl>
2025-09-12 15:20:32 +0200

    workflows aangepast

--- js/config.js
+++ js/config.js
@@ -27,8 +27,8 @@ let respecConfig = {
   publishVersion: "3.0",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  //previousPublishDate: null ,
-  //previousMaturity: null,
+  previousPublishDate: null ,
+  previousMaturity: null,
   prevVersion: ["https://geonovum.github.io/TPOD/CIMOW/CIMOW_v2.2.0.pdf"],
   //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
